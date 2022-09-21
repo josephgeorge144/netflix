8be9139b183ba1ccba10326444267db65b34e28e
Joseph George Venmelil <84468450+josephgeorge144@users.noreply.github.com>
2022-09-21 22:25:09 +0100

    banner, posters added

---
+++ src/Components/Banner/Banner.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Banner.css'
+
+function Banner() {
+  return (
+    <div className='banner'>
+        <div className='content'>
+            <h1 className='title'>Movie Name</h1>
+            <div className='banner_buttons'>
+                <button className='button'>Play</button>
+                <button className='button'>My List</button>
+                
+            </div>
+            <h1 className='discription'>This is another one of TOP banks in the UK; the second largest. It was established in 2009 from the merger of HBOS  </h1>
+        </div>
+        
+        
+        </div>
+  )
+}
+
+export default Banner
